9f052711380163776adbd1937630ac4be48196e9
Paul <Paul@10.0.0.178>
2020-04-09 22:55:36 -0400

    Need to work on FSM

--- homework4.c
+++ homework4.c
@@ -1,5 +1,6 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "homework4.h"
+#include <ti/devices/msp432p4xx/driverlib/gpio.h>
 
 int main(void)
 {
@@ -8,32 +9,66 @@ int main(void)
 
     // TODO: Declare the variables that main uses to interact with your state machine.
 
-
+    bool finished = false;
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
+    typedef struct _eUSCI_eUSCI_UART_ConfigV1
+    {
+        uint_fast8_t selectClockSource;
+        uint_fast16_t clockPrescalar;
+        uint_fast8_t firstModReg;
+        uint_fast8_t secondModReg;
+        uint_fast8_t parity;
+        uint_fast16_t msborLsbFirst;
+        uint_fast16_t numberofStopBits;
+        uint_fast16_t uartMode;
+        uint_fast8_t overSampling;
+        uint_fast16_t dataLength;
+    } eUSCI_UART_ConfigV1;
+    const eUSCI_UART_ConfigV1 uartconfig =
+    {
+             EUSCI_A_UART_CLOCKSOURCE_SMCLK,              // Onboard clock
+             19,
+             8,
+             0x55,
+             EUSCI_A_UART_NO_PARITY,                       // No Parity
+             EUSCI_A_UART_LSB_FIRST,                       // LSB First
+             EUSCI_A_UART_ONE_STOP_BIT,                    // One stop bit
+             EUSCI_A_UART_MODE,                            // UART mode
+             EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,// Oversampling
+             EUSCI_A_UART_8_BIT_LEN
+
+    };
 
 
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
-
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3, GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P3, GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     // TODO: Initialize EUSCI_A0
 
+    UART_initModule(EUSCI_A0_BASE, &uartconfig); //Not sure why this is an invalid argument?
 
     // TODO: Enable EUSCI_A0
-
+    UART_enableModule(EUSCI_A0_BASE);
 
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
-
-
+        if(UART_getInterruptStatus(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG) == EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+            rChar = UART_receiveData(EUSCI_A0_BASE);
+        else
+            rChar = 0xFF;
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
         //       Check the transmit interrupt flag prior to transmitting the character.
 
-
+        if(rChar != 0xFF)
+        {
+            charFSM(rChar);
+        }
 
         // TODO: If the FSM indicates a successful string entry, transmit the response string.
         //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
